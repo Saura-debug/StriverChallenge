25a0ac4133fc7a06b970a023aa56ba4fa1d42c43
sudhanshu <sp7017184@gmail.com>
2026-06-25 10:46:10 +0530

    buyandsell

---
+++ buyandsell.java
@@ -0,0 +1,10 @@
+public class buyandsell {
+    public static void main(String[] args) {
+
+        // this question was quite intresting and easy 
+        // here for optimal,I kept minimum while selling and that's how i got my answer
+        
+     
+    }
+    
+}
